c07b4fc6127e3c6bb79380f3bfbe5b79b4d0b246
yushenko-diana <47981922+yushenko-diana@users.noreply.github.com>
2020-09-23 17:49:29 +0300

    Add files via upload

--- src/main/main.c
+++ src/main/main.c
@@ -2,9 +2,6 @@
 
 int volatile num1, num2, result, i;
 int N = 5;
-float a = 1.2;      // адрес: 0x20000014
-double b = 34.4;  // адрес: 0x20000018
-int c = 234;    // адрес: 0x20000020
 int main()
 {
     num1 = 0;
