5dd2fdf0b89f4c8cf70e0c0eb9b216cc1aa58285
yushenko-diana <47981922+yushenko-diana@users.noreply.github.com>
2020-09-23 17:47:17 +0300

    Add files via upload

--- src/main/main.c
+++ src/main/main.c
@@ -1,7 +1,18 @@
 #include <stm32f10x.h>
 
-int main(void)
+int volatile num1, num2, result, i;
+int N = 5;
+float a = 1.2;      // адрес: 0x20000014
+double b = 34.4;  // адрес: 0x20000018
+int c = 234;    // адрес: 0x20000020
+int main()
 {
+    num1 = 0;
+    num2 = 1;
+    for (i = 1; i < N; i++) {
+        result = num1 + num2;
+        num1 = num2;
+        num2 = result;
+    }
     return 0;
 }
-
